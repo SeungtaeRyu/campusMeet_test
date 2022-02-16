67628d2c14d603e282d573ca6c1e50e30f6aeb64
서해수 <seohaesu@seohaesuui-MacBookAir.local>
2022-02-16 23:48:09 +0900

    s

--- campus_meet_test/lib/screens/my_meeting_screen.dart
+++ campus_meet_test/lib/screens/my_meeting_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
-
+ // 페이지 2분할
 class MyMeetingScreen extends StatefulWidget {
+
   const MyMeetingScreen({Key? key}) : super(key: key);
 
   @override
@@ -10,6 +11,8 @@ class MyMeetingScreen extends StatefulWidget {
 class _MyMeetingScreenState extends State<MyMeetingScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+
+    );
   }
 }
